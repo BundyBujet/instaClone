63a5a3c1ccc850bf1855f27c2b8660e2c7b5e096
BundyBujet <hossam3wad88919@gmail.com>
2024-01-25 00:36:10 -0500

    fixed query key

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -162,7 +162,7 @@ export const useDeletePost = () => {
       deletePost(postId, imageId),
     onSuccess: () =>
       queryClient.invalidateQueries({
-        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
       }),
   });
 };
